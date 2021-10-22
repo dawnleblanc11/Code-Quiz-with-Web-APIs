fc912ec498824372baa82a1f4d187a76dfa82ee0
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-10-22 11:18:08 -0400

    progress on errors with highscore page

--- assets/js/scripths.js
+++ assets/js/scripths.js
@@ -2,10 +2,17 @@ var restartquizButtonEl = document.getElementById('restartquiz');
 var clearhighscoresButton = document.getElementById('clearhighscores');
 
 // not sure if relaod is the righ function here
-restartquizButtonEl.addEventListener("click", location.reload());
-clearhighscoresButton.addEventListener("click", initalizescoresarray);
+restartquizButtonEl.addEventListener("click", testbuttons);
+clearhighscoresButton.addEventListener("click", testbuttons);
+
+//restartquizButtonEl.addEventListener("click", location.reload());
+//clearhighscoresButton.addEventListener("click", initalizescoresarray);
 
 function restart() {
     window.history.back();
     location.reload();
+}
+
+function testbuttons() {
+    console.log("clickworked");
 }
